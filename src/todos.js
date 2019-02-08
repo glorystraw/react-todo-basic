d8495ca892bd836f6c3bcf32814aa33cb074d9a2
glorystraw <glorystraw@users.noreply.github.com>
2019-02-08 18:15:18 +0300

    Todo App on React

--- src/todos.js
+++ src/todos.js
@@ -1,19 +1,19 @@
 const todos = [
-    {
-        id: 1,
-        title: 'Do app on React',
-        completed: true
-    },
-    {
-        id: 2,
-        title: 'Add Redux to app',
-        completed: false
-    },
-    {
-        id: 3,
-        title: 'Add  Saga to app',
-        completed: false
-    }
+    // {
+    //     id: 1,
+    //     title: 'Do app on React',
+    //     completed: true
+    // },
+    // {
+    //     id: 2,
+    //     title: 'Add Redux to app',
+    //     completed: false
+    // },
+    // {
+    //     id: 3,
+    //     title: 'Add  Saga to app',
+    //     completed: false
+    // }
 ];
 
 export default todos;
